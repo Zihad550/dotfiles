75535fd7327904c74514eb24456325a0bea72b8f
Zihad550 <dev.jehadhossain@gmail.com>
2026-05-02 20:55:59 +0600

    feat: Bind mount dotfiles in devcontainer
    Bind mount the local dotfiles directory into the devcontainer. Configure `install.sh` to execute as a `postCreateCommand`.
    
    Remove commented-out `nix` feature and custom `setup-devcontainer` configurations. Make the `stow` setup script executable. Add `mise trust` and remove `neovim` installation from `install.sh`. Stop installing `lazygit` via `mise` in the `tools` script.

--- setup/devcontainer/install.sh
+++ setup/devcontainer/install.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
 sudo apt update -y
-sudo apt upgrade -y
+# sudo apt upgrade -y
 
-sudo apt install -y stow neovim
+sudo apt install -y stow
 # eza trash-cli zsh
 $HOME/dotfiles/setup/devcontainer/stow
+mise trust
 $HOME/dotfiles/setup/devcontainer/tools
 
 # sudo chsh -s $(which zsh) $USER
