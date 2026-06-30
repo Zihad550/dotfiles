75657b4f352fe081ca2522e2ea5fb457228c3b37
Zihad550 <dev.jehadhossain@gmail.com>
2026-06-30 08:55:36 +0600

    chore: Update devcontainer environment and installation scripts
    Upgrade the base image to Ubuntu 26.04, disable Moby for Docker, and
    streamline dependency installation by switching to PPA-based packages
    for mise and Neovim.

--- setup/devcontainer/install.sh
+++ setup/devcontainer/install.sh
@@ -3,19 +3,22 @@
 sudo apt update -y
 sudo apt upgrade -y
 
-sudo apt install -y zsh eza fzf ripgrep trash-cli bat bubblewrap socat git-delta
+sudo apt install -y zsh eza fzf ripgrep trash-cli bat bubblewrap socat git-delta software-properties-common starship
+
+# mise
+sudo add-apt-repository -y ppa:jdxcode/mise
+sudo apt update -y
+sudo apt install -y mise
+
 # neovim
-curl https://mise.run | sh
-# sudo add-apt-repository -y ppa:jdxcode/mise
-# sudo apt update -y
-# sudo apt install -y mise
+sudo add-apt-repository ppa:neovim-ppa/unstable -y
+sudo apt update -y
+sudo apt install -y make gcc ripgrep fd-find tree-sitter-cli unzip git xclip neovim
 
 $HOME/dotfiles/setup/devcontainer/stow
 mise trust
 $HOME/dotfiles/setup/devcontainer/tools
 
 sudo chsh -s $(which zsh) $USER
-
-curl -sS https://starship.rs/install.sh | sh
 
 echo "now logout and log back in"
